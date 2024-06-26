cc2ee000d71c0646e36dbdc276438b575b33d25e
Filipe Campos <filipepcampos24@gmail.com>
2024-06-26 13:18:57 +0000

    bug fix centralized dp

--- src/train_centralized_dp.py
+++ src/train_centralized_dp.py
@@ -1,16 +1,20 @@
 import argparse
 
 import torchvision
+import torch
+import torch.nn as nn
+import torch.optim as optim
 import lightning as L
-from lightning.pytorch import callbacks as pl_callbacks
-from lightning.pytorch import loggers as pl_loggers
+from opacus import PrivacyEngine
+from opacus.validators import ModuleValidator
+from tqdm import tqdm
+import numpy as np
 
 from data.mimic_cxr_jpg import MIMICCXRDataModule
 from data.chexpert import ChexpertDataModule
 from data.brax import BraxDataModule
 
 from models.densenet import DenseNet121
-from modules.explainable_classifier import ExplainableClassifier
 
 L.pytorch.seed_everything(42, workers=True)
 
@@ -38,15 +42,15 @@ def get_datamodule(dataset: str, batch_size: int):
         )
     raise ValueError(f"Unknown dataset: {dataset}")
 
+
 def train(model, device, train_loader, optimizer, privacy_engine, epoch):
     model.train()
-    criterion = nn.CrossEntropyLoss()
 
     losses = []
     for _batch_idx, (x, y) in enumerate(tqdm(train_loader)):
         optimizer.zero_grad()
 
-        prediction = self.model(x)
+        prediction = model(x)
         prediction = nn.functional.sigmoid(prediction).squeeze()
         loss = nn.functional.binary_cross_entropy(prediction.float(), y.float())
         loss.backward()
@@ -54,29 +58,32 @@ def train(model, device, train_loader, optimizer, privacy_engine, epoch):
         optimizer.step()
         losses.append(loss.item())
 
-        epsilon = privacy_engine.accountant.get_epsilon(delta=args.delta)
+        epsilon = privacy_engine.accountant.get_epsilon(delta=1e-5)
         print(
             f"Train Epoch: {epoch} \t"
             f"Loss: {np.mean(losses):.6f} "
-            f"(ε = {epsilon:.2f}, δ = {args.delta})"
+            f"(ε = {epsilon:.2f}, δ = {1e-5})",
         )
 
 
 def test(model, device, test_loader):
     model.eval()
-    criterion = nn.CrossEntropyLoss()
     test_loss = 0
     correct = 0
     with torch.no_grad():
         for x, y in tqdm(test_loader):
             x, y = x.to(device), y.to(device)
             output = model(x)
-            output = nn.functional.sigmoid(prediction).squeeze()
-            test_loss += nn.functional.binary_cross_entropy(output.float(), y.float()).item()
+            output = nn.functional.sigmoid(output).squeeze()
+            test_loss += nn.functional.binary_cross_entropy(
+                output.float(),
+                y.float(),
+            ).item()
             pred = output.argmax(
-                dim=1, keepdim=True
+                dim=1,
+                keepdim=True,
             )  # get the index of the max log-probability
-            correct += pred.eq(target.view_as(pred)).sum().item()
+            correct += pred.eq(y.view_as(pred)).sum().item()
 
     test_loss /= len(test_loader.dataset)
 
@@ -86,29 +93,35 @@ def test(model, device, test_loader):
             correct,
             len(test_loader.dataset),
             100.0 * correct / len(test_loader.dataset),
-        )
+        ),
     )
     return correct / len(test_loader.dataset)
+
 
 def main():
     args = parser.parse_args()
 
     model = DenseNet121(weights=torchvision.models.DenseNet121_Weights.IMAGENET1K_V1)
+    model = ModuleValidator.fix(model)
     optimizer = optim.Adam(model.parameters(), lr=1e-4)
 
     datamodule = get_datamodule(args.dataset, args.batch_size)
     privacy_engine = PrivacyEngine()
-   
+
     model, optimizer, train_loader = privacy_engine.make_private(
         module=model,
         optimizer=optimizer,
         data_loader=datamodule.train_dataloader(),
         noise_multiplier=1.3,
-        max_grad_norm=1.0
+        max_grad_norm=1.0,
     )
 
+    device = "cuda"
+
     for epoch in range(1, 11):
-        train(model,  device, train_loader, optimizer, privacy_engine, epoch)
+        train(model, device, train_loader, optimizer, privacy_engine, epoch)
+
+    test_loader = datamodule.test_dataloader()
     test_results = test(model, device, test_loader)
     print(test_results)
 
